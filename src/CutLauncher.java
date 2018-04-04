2a60c272f20740c38944a0adab24d172353cba6c
KamikadzeBull <kenny1634tlt@yandex.ru>
2018-04-04 14:19:46 +0300

    Теперь пути до файла извлекаются правильно

--- src/CutLauncher.java
+++ src/CutLauncher.java
@@ -6,13 +6,14 @@ public class CutLauncher {
 
     public static void main(String[] args) throws Exception{
 
-        String inputFilePath;
-        String outputFilePath;
+        final String inputFilePath;
+        final String outputFilePath;
         boolean charIndentation = false;
-
+        final String commandLineFormat = "\n[-c|-w] \"input file path\" range" +
+                "\n[-c|-w] -o \"output file path\" \"input file path\" range";
 
         if (args.length < 3)
-            throw new Exception("Wrong command line");
+            throw new Exception(commandLineFormat);
         if (args[0].equals("-c"))
             charIndentation = true;
         else {
@@ -22,44 +23,42 @@ public class CutLauncher {
         }
 
 
-        Pattern filePathFormat =
-                Pattern.compile("(([A-Z]:\\\\(([^\\\\/:*?\"<>|]+)\\\\)+)[^\\\\/:*?\"<>|]+.txt)" + "|"
-                        + "((([^\\\\/:*?\"<>|]+)/)+[^\\\\/:*?\"<>|]+.txt)");
-        Pattern filePathsFormat =
-                Pattern.compile("^((([A-Z]:\\\\(([^\\\\/:*?\"<>|]+)\\\\)+)[^\\\\/:*?\"<>|]+.txt\\s?)" + "|"
-                        + "((([^\\\\/:*?\"<>|]+)/)+[^\\\\/:*?\"<>|]+.txt\\s?)){1,2}$");
-        Matcher matcher;
-        StringBuilder filePathsBuilder = new StringBuilder("");
-        String filePaths;
-        List<String> filePathsList = new ArrayList<>();
+        Pattern fileAbsolutePathFormat =
+                Pattern.compile("([A-Z]:\\\\(([^\\\\/:*?\"<>|]+)\\\\)+)[^\\\\/:*?\"<>|]+.txt");
+        Pattern fileRelativePathFormat =
+                Pattern.compile("(([^\\\\/:*?\"<>|]+)/)+[^\\\\/:*?\"<>|]+.txt");
+        Matcher matcher1;
+        Matcher matcher2;
+        List<String> filePaths = new ArrayList<>();
 
         if (args[1].equals("-o"))
             for (int i = 2; i < args.length-1; i++)
-                filePathsBuilder.append(args[i]).append(" ");
+                filePaths.add(args[i]);
         else
             for (int i = 1; i < args.length-1; i++)
-                filePathsBuilder.append(args[i]).append(" ");
+                filePaths.add(args[i]);
 
-        filePaths = filePathsBuilder.toString();
-        matcher = filePathsFormat.matcher(filePaths);
-        if (!matcher.matches())
-            throw new Exception("Wrong command line");
-        matcher = filePathFormat.matcher(filePaths);
-        while (matcher.find())
-            filePathsList.add(matcher.group());
+        for (String filePath: filePaths){
+            matcher1 = fileAbsolutePathFormat.matcher(filePath);
+            matcher2 = fileRelativePathFormat.matcher(filePath);
+            if (!matcher1.matches() && !matcher2.matches())
+                throw new Exception(commandLineFormat);
+        }
 
         switch (args[1]){
             case "-o":
-                if (filePathsList.size() != 2)
-                    throw new Exception("Wrong command line");
-                outputFilePath = filePathsList.get(0);
-                inputFilePath = filePathsList.get(1);
+                if (filePaths.size() != 2)
+                    throw new Exception(commandLineFormat);
+                outputFilePath = filePaths.get(0);
+                inputFilePath = filePaths.get(1);
+                if (outputFilePath.equals(inputFilePath))
+                    throw new Exception("Paths are same");
                 break;
             default:
-                if (filePathsList.size() != 1)
+                if (filePaths.size() != 1)
                     throw new Exception("Wrong command line");
                 outputFilePath = null;
-                inputFilePath = filePathsList.get(0);
+                inputFilePath = filePaths.get(0);
                 break;
         }
 
@@ -78,20 +77,20 @@ public class CutLauncher {
         Pattern format3 = Pattern.compile("(\\d+)-(\\d+)");
 
         while (true) {
-            matcher = format1.matcher(args[args.length-1]);
-            if (matcher.matches()) {
+            matcher1 = format1.matcher(args[args.length-1]);
+            if (matcher1.matches()) {
                 if (digitsMatcher.find())
                     range[0] = Integer.parseInt(digitsMatcher.group());
                 break;
             }
-            matcher = format2.matcher(args[args.length-1]);
-            if (matcher.matches()) {
+            matcher1 = format2.matcher(args[args.length-1]);
+            if (matcher1.matches()) {
                 if (digitsMatcher.find())
                     range[1] = Integer.parseInt(digitsMatcher.group());
                 break;
             }
-            matcher = format3.matcher(args[args.length-1]);
-            if (matcher.matches()) {
+            matcher1 = format3.matcher(args[args.length-1]);
+            if (matcher1.matches()) {
                 int i = 0;
                 while (digitsMatcher.find()){
                     range[i] = Integer.parseInt(digitsMatcher.group());
@@ -105,5 +104,6 @@ public class CutLauncher {
         }
 
         Cut cut = new Cut(charIndentation, outputFilePath, inputFilePath, range);
+        cut.toCut();
     }
 }
