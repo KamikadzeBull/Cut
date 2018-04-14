ff64b999578d664683710958e6647b844df4e10e
KamikadzeBull <kenny1634tlt@yandex.ru>
2018-04-14 17:45:53 +0300

    ✂

--- src/CutLauncher.java
+++ src/CutLauncher.java
@@ -1,68 +1,41 @@
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
+import org.kohsuke.args4j.*;
 import java.util.regex.*;
 
 public class CutLauncher {
 
+    @Option(name = "-c", usage = "char indentation")
+    private boolean charIndentation;
+
+    @Option(name = "-w", usage = "word indentation")
+    private boolean wordIndentation;
+
+    @Option(name = "-o", usage = "output file path")
+    private String outputFilePath = "";
+
+    @Argument (usage = "input file path")
+    private String inputFilePath = "";
+
+    @Argument (usage = "indentation range")
+    private String range = "";
+
+
     public static void main(String[] args) throws Exception{
+        new CutLauncher().launch(args);
+    }
 
-        final String inputFilePath;
-        final String outputFilePath;
-        boolean charIndentation = false;
-        final String commandLineFormat = "\n[-c|-w] \"input file path\" range" +
-                "\n[-c|-w] -o \"output file path\" \"input file path\" range";
+    private void launch(String[] args) throws Exception{
 
-        if (args.length < 3)
-            throw new Exception(commandLineFormat);
-        if (args[0].equals("-c"))
-            charIndentation = true;
-        else {
-            /* charIndentation == false ~ wordIndentation == true */
-            if (!args[0].equals("-w"))
-                throw new Exception("Wrong indentation flag");
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            e.printStackTrace();
+            return;
         }
 
-
-        Pattern fileAbsolutePathFormat =
-                Pattern.compile("([A-Z]:\\\\(([^\\\\/:*?\"<>|]+)\\\\)+)[^\\\\/:*?\"<>|]+.txt");
-        Pattern fileRelativePathFormat =
-                Pattern.compile("(([^\\\\/:*?\"<>|]+)/)+[^\\\\/:*?\"<>|]+.txt");
-        Matcher matcher1;
-        Matcher matcher2;
-        List<String> filePaths = new ArrayList<>();
-
-        if (args[1].equals("-o"))
-            for (int i = 2; i < args.length-1; i++)
-                filePaths.add(args[i]);
-        else
-            for (int i = 1; i < args.length-1; i++)
-                filePaths.add(args[i]);
-
-        for (String filePath: filePaths){
-            matcher1 = fileAbsolutePathFormat.matcher(filePath);
-            matcher2 = fileRelativePathFormat.matcher(filePath);
-            if (!matcher1.matches() && !matcher2.matches())
-                throw new Exception(commandLineFormat);
-        }
-
-        switch (args[1]){
-            case "-o":
-                if (filePaths.size() != 2)
-                    throw new Exception(commandLineFormat);
-                outputFilePath = filePaths.get(0);
-                inputFilePath = filePaths.get(1);
-                if (outputFilePath.equals(inputFilePath))
-                    throw new Exception("Paths are same");
-                break;
-            default:
-                if (filePaths.size() != 1)
-                    throw new Exception("Wrong command line");
-                outputFilePath = null;
-                inputFilePath = filePaths.get(0);
-                break;
-        }
-
+        if ((charIndentation && wordIndentation) ||
+                (!charIndentation && !wordIndentation))
+            throw new IllegalArgumentException();
 
         int[] range = {-1, -1};
         Pattern digits = Pattern.compile("\\d+");
@@ -76,39 +49,35 @@ public class CutLauncher {
         Pattern format1 = Pattern.compile("(\\d+)-");
         Pattern format2 = Pattern.compile("-(\\d+)");
         Pattern format3 = Pattern.compile("(\\d+)-(\\d+)");
+        Matcher matcher;
 
-        while (true) {
-            matcher1 = format1.matcher(args[args.length-1]);
-            if (matcher1.matches()) {
-                if (digitsMatcher.find())
-                    range[0] = Integer.parseInt(digitsMatcher.group());
-                break;
-            }
-            matcher1 = format2.matcher(args[args.length-1]);
-            if (matcher1.matches()) {
+        matcher = format1.matcher(args[args.length-1]);
+        if (matcher.matches()) {
+            if (digitsMatcher.find())
+                range[0] = Integer.parseInt(digitsMatcher.group());
+        }
+        else {
+            matcher = format2.matcher(args[args.length-1]);
+            if (matcher.matches()) {
                 if (digitsMatcher.find())
                     range[1] = Integer.parseInt(digitsMatcher.group());
-                break;
             }
-            matcher1 = format3.matcher(args[args.length-1]);
-            if (matcher1.matches()) {
-                int i = 0;
-                while (digitsMatcher.find()){
-                    range[i] = Integer.parseInt(digitsMatcher.group());
-                    i++;
+            else {
+                matcher = format3.matcher(args[args.length-1]);
+                if (matcher.matches()) {
+                    int i = 0;
+                    while (digitsMatcher.find()){
+                        range[i] = Integer.parseInt(digitsMatcher.group());
+                        i++;
+                    }
+                    if (range[0] >= range[1])
+                        throw new Exception("First number has to be less than second");
                 }
-                if (range[0] >= range[1])
-                    throw new Exception("First number has to be less than second");
-                break;
+                else
+                    throw new Exception("Wrong format of range");
             }
-            throw new Exception("Wrong format of range");
         }
 
-        try {
-            new Cut(charIndentation, outputFilePath, inputFilePath, range).toCut();
-        }
-        catch (FileNotFoundException e) {
-            throw new Exception("Input file does not exist");
-        }
+        Cut.toCut(charIndentation, outputFilePath, inputFilePath, range);
     }
 }
